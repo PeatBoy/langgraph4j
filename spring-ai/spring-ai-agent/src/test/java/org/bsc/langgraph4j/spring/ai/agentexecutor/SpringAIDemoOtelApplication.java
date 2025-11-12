cdd75ad6add950d2f1c0ecb9cf492b5eb6fffa38
bsorrentino <bartolomeo.sorrentino@gmail.com>
2025-11-12 12:06:07 +0100

    refactor(sprint-ai-agent): use  opentelemetry-spring-boot-starter
    work on [#271](https://github.com/langgraph4j/langgraph4j/issues/271)

--- spring-ai/spring-ai-agent/src/test/java/org/bsc/langgraph4j/spring/ai/agentexecutor/SpringAIDemoOtelApplication.java
+++ spring-ai/spring-ai-agent/src/test/java/org/bsc/langgraph4j/spring/ai/agentexecutor/SpringAIDemoOtelApplication.java
@@ -1,16 +1,18 @@
 package org.bsc.langgraph4j.spring.ai.agentexecutor;
 
-import io.opentelemetry.sdk.OpenTelemetrySdk;
-import org.springframework.beans.factory.annotation.Autowired;
+import io.opentelemetry.api.GlobalOpenTelemetry;
+import io.opentelemetry.api.OpenTelemetry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringAIDemoOtelApplication {
 
-    @Autowired
-    OpenTelemetrySdk openTelemetrySdk;
 
+    SpringAIDemoOtelApplication( OpenTelemetry openTelemetrySdk ) {
+
+        GlobalOpenTelemetry.set( openTelemetrySdk );
+    }
     public static void main(String[] args) {
 
         SpringApplication.run( SpringAIDemoOtelApplication.class, args );
