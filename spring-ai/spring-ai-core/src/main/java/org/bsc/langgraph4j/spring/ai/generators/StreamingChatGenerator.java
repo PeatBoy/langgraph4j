6dc4fa7aee03a1513308ce56a3051d2f965e79a5
bsorrentino <bartolomeo.sorrentino@gmail.com>
2025-11-13 11:11:35 +0100

    refactor(spring-ai): bump spring-ai from 1.0.3 to version 1.1.0

--- spring-ai/spring-ai-core/src/main/java/org/bsc/langgraph4j/spring/ai/generators/StreamingChatGenerator.java
+++ spring-ai/spring-ai-core/src/main/java/org/bsc/langgraph4j/spring/ai/generators/StreamingChatGenerator.java
@@ -94,14 +94,15 @@ public interface StreamingChatGenerator {
 
                     final var currentMessageText = currentMessage.getText();
 
-                    var newMessage =  new AssistantMessage(
+                    var newMessage =  AssistantMessage.builder()
+                            .content(
                             currentMessageText != null ?
                                     lastMessageText.concat( currentMessageText ) :
-                                    lastMessageText,
-                            currentMessage.getMetadata(),
-                            toolCalls,
-                            currentMessage.getMedia()
-                    );
+                                    lastMessageText)
+                            .properties(currentMessage.getMetadata())
+                            .toolCalls(currentMessage.getToolCalls())
+                            .media(currentMessage.getMedia())
+                            .build();
 
                     var newGeneration = new Generation(newMessage, response.getResult().getMetadata());
                     return new ChatResponse( List.of(newGeneration), response.getMetadata());
